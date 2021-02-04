a174c7bfde51f61ed716b62e671b4f1008abd720
S Reddy <sachitgreddy@email.com>
2021-02-04 00:24:16 -0500

    Added my 1st loop & started inner loop from nober3

--- noober.js
+++ noober.js
@@ -7,6 +7,25 @@ async function pageLoaded() {
   
   // 🔥 start here: write code to loop through the rides
   
+  // First loop that goes through all he rides and gives access to each ride
+  for (let i = 0; i<json.length; i++) {
+    let ride = json[i]
+    console.log(ride)
+  
+  // Inner loop that goes through each individual ride's array of legs and gives you access to each individual leg
+
+    if (ride.length > 1) {
+      levelOfService = 'Noober Pool'
+    } else if (ride[0].purpleRequested) {
+      levelOfService = 'Noober Purple'
+    } else if (ride[0].numberOfPassengers > 3) {
+      levelOfService = 'Noober XL'
+    } else {
+      levelOfService = 'Noober X'
+    }
+
+  }
+
   
 
 }
